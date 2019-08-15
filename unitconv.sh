74701919f5c34c1d50c3fe2df4582a60ce4c1a74
Daniel Haase <hs.dnl@web.de>
2019-08-15 14:18:33 +0200

    removed blctl.sh inet.sh unitconv.sh userlist.sh volctl.sh

--- unitconv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-which units &> /dev/null
-if [ $? -ne 0 ]; then echo "command \"units\" not installed on the system"; exit 2; fi
-if [ $# -ne 3 ]; then echo "usage: $(basename $0) <value> <from-unit> <to-unit>"; exit 1; fi
-units --compact --one-line "$1 $2" $3
-exit 0
