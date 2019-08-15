d14ffd7eb77d4a040ccc72e235cd5bd12b47868c
Daniel Haase <hs.dnl@web.de>
2019-08-15 18:58:39 +0200

    updated aphwaddr.sh and README

--- aphwaddr.sh
+++ aphwaddr.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 <interface>"
-	exit 1
-else
-	INTERFACE=$1
-fi
+which ip &> /dev/null
+if [ $? -ne 0 ]; then echo "command \"ip\" not found"; exit 2; fi
 
 which iw &> /dev/null
+if [ $? -ne 0 ]; then echo "command \"iw\" not found"; exit 2; fi
 
-if [ $? -eq 0 ]; then
-	PROFILE=$(netctl list | grep '*' | awk '{print $2}')
-
-	if [ "$PROFILE" == "" ] || [ "$PROFILE" == " " ]; then
-		echo "no network connection found."
-		exit 2
-	else
-		HWADDR=$(iw dev $INTERFACE station dump -v | grep "Station" | awk '{print $2}')
-
-		if [ "$HWADDR" != "" ] && [ "$HWADDR" != " " ]; then
-			echo "$HWADDR"
-			exit 0
-		else
-			echo "failed to get hardware address of wireless AP on interface $INTERFACE."
-			exit 3
-		fi
-	fi
+if [ $# -eq 0 ]; then INTERFACE=$(ip route | awk '/^default/ {print $5}')
+elif [ $# -eq 1 ]; then INTERFACE=$1
 else
-	echo "command \"iw\" not found."
-	exit 4
+	echo "usage:  $(basename $0) [<interface>]"
+	exit 1
 fi
+
+HWADDR=$(iw dev $INTERFACE station dump | grep "Station" | awk '{print $2}')
+
+if [ "$HWADDR" != "" ] && [ "$HWADDR" != " " ]; then
+	echo "$HWADDR"
+	exit 0
+else
+	echo "failed to get hardware address of wireless AP on interface $INTERFACE"
+	exit 3
+fi
+
+exit 0
+
 
 
