60b434a32a557edf22c9f573d20fb46578cd09b5
Daniel Haase <hs.dnl@web.de>
2021-06-29 16:10:26 +0200

    Update "invoke.sh"

--- invoke.sh
+++ invoke.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env -S bash
 ##
 ## invoke - start background process and suppress any output
-## Copyright (C) 2020 Daniel Haase
+## Copyright (C) 2020-2021 Daniel Haase
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -17,11 +17,14 @@
 ## along with this program. If not, see <http://www.gnu.org/licenses/gpl.txt>.
 ##
 
+set -euo pipefail
+
 if [ $# -eq 0 ] || [ -z "$1" ]; then exit 0; fi
 while [ "$1" == "invoke" ]; do shift; done
 
-which "$1" &> /dev/null
-if [ $? -ne 0 ]; then echo "no such command \"$1\""; exit 1; fi
+if [ $# -eq 0 ] || [ -z "$1" ]; then exit 0
+elif ! command -v "$1" &> /dev/null
+then echo "no such command \"$1\""; exit 1; fi
 
-$($@ &> /dev/null &)
+eval "$@" &> /dev/null &
 exit 0
