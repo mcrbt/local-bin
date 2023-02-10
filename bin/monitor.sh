89f5761ed01350e377d300e550a15d47579f2191
Daniel Haase <hs.dnl@web.de>
2023-02-10 19:16:37 +0100

    Update "monitor.sh"
    Signed-off-by: Daniel Haase <hs.dnl@web.de>

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ##
-## monitor - quick information about connected monitors
-## Copyright (C) 2020 Daniel Haase
+## monitor - brief information about connected monitors
+## Copyright (C) 2020, 2023 Daniel Haase
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -17,76 +17,106 @@
 ## along with this program. If not, see <http://www.gnu.org/licenses/gpl.txt>.
 ##
 
-APP="$0"
+set -o errexit
+set -o nounset
+set -o pipefail
+
+APP="$(basename "${0}")"
 VERSION="0.1.0"
 
-function checkcmd
+function check_command
 {
-  local c="$1"
-  if [ $# -eq 0 ] || [ -z "$c" ]; then return 0; fi
-  which "$c" &> /dev/null
-  if [ $? -ne 0 ]; then echo "command \"$c\" not found"; exit 1; fi
-  return 0
+	local command="${1}"
+
+	if [[ $# -eq 0 || -z "${command}" ]] \
+	|| command -v "${command}" &>/dev/null; then
+		return 0
+	else
+		echo "no such command \"${command}\""
+		exit 1
+	fi
 }
 
 function list
 {
-  xrandr --listmonitors | awk '/\+/ {print $4}'
+	xrandr --listmonitors | awk '/\+/ {print $4}'
 }
 
 function count
 {
-  xrandr --listmonitors | awk '/^Monitor/ {print $2}'
+	xrandr --listmonitors | awk '/^Monitor/ {print $2}'
 }
 
 function version
 {
-  echo "$APP version $VERSION"
-  echo " - quick information about connected monitors"
-  echo "copyright (C) 2020 Daniel Haase"
+	cat <<-EOF
+	${APP} version ${VERSION}
+	 - brief information about connected monitors
+	copyright (C) 2020, 2023 Daniel Haase
+	EOF
 }
 
-function help
+function usage
 {
-  version
-  echo ""
-  echo "usage:  $APP [list | count | version | help]"
-  echo ""
-  echo "  list | --list | -l"
-  echo "    list names of detected monitor interfaces"
-  echo ""
-  echo "  count | --count | -c"
-  echo "    print number of detected monitors"
-  echo ""
-  echo "  version | --version | -V"
-  echo "    print version information"
-  echo ""
-  echo "  help | --help | -h"
-  echo "    print this help message"
-  echo ""
+	version
+
+	cat <<-EOF
+
+	usage:  ${APP} [list | count | version | help]
+
+	   list | --list | -l
+	      list names of detected monitor interfaces
+
+	   count | --count | -c
+	      print number of detected monitors
+
+	   version | --version | -V
+	      print version information and exit
+
+	   help | --help | -h
+	      print this help message and exit
+
+	EOF
 }
 
-checkcmd "awk"
-checkcmd "basename"
-checkcmd "xrandr"
+check_command "awk"
+check_command "basename"
+check_command "cat"
+check_command "xrandr"
 
-APP="$(basename $APP)"
-
-if [ "$APP" == "monitorlist" ] || [ "$APP" == "monlist" ]
-then list; exit 0
-elif [ "$APP" == "monitorcount" ] || [ "$APP" == "moncount" ]
-then count; exit 0
-elif [ "$APP" == "monitor" ] || [ "$APP" == "mon" ]; then
-  if [ $# -eq 1 ]; then
-    if [ "$1" == "list" ] || [ "$1" == "--list" ] || [ "$1" == "-l" ]
-    then list; exit 0
-    elif [ "$1" == "count" ] || [ "$1" == "--count" ] || [ "$1" == "-c" ]
-    then count; exit 0
-    elif [ "$1" == "version" ] || [ "$1" == "--version" ] || [ "$1" == "-V" ]
-    then version; exit 0
-    elif [ "$1" == "help" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ]
-    then help; exit 0; fi
-  else help; exit 2; fi
-else echo "unkown script name \"$APP\""; exit 3; fi
+case "${APP}" in
+	monitorlist|monlist|monitorlist.sh|monlist.sh)
+		list
+		;;
+	monitorcount|moncount|monitorcount.sh|moncount.sh)
+		count
+		;;
+	monitor|mon|monitor.sh|mon.sh)
+		if [[ $# -eq 1 ]]; then
+			case "${1}" in
+				list|--list|-l)
+					list
+					;;
+				count|--count|-c)
+					count
+					;;
+				version|--version|-V)
+					version
+					;;
+				help|--help|-h)
+					usage
+					;;
+				*)
+					usage
+					exit 2
+					;;
+			esac
+		fi
+		;;
+	*)
+		echo "unknown script name \"${APP}\""
+		exit 3
+		;;
+esac
 
 exit 0
