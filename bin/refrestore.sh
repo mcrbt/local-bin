c2cb6362b0558ef3c352af5687bf160a9e53c08a
Daniel Haase <hs.dnl@web.de>
2023-03-31 01:07:32 +0200

    Rewrite script "refrestore.sh"
    Signed-off-by: Daniel Haase <hs.dnl@web.de>

--- bin/refrestore.sh
+++ bin/refrestore.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ##
 ## refrestore - reopen hyperlinks read from ASCII file in firefox
-## Copyright (C) 2020 Daniel Haase
+## Copyright (C) 2020-2023  Daniel Haase
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -17,88 +17,174 @@
 ## along with this program. If not, see <http://www.gnu.org/licenses/gpl.txt>.
 ##
 
-VERSION="0.2.0"
-CWD=$(pwd)
+# shellcheck disable=SC2310
 
-function checkcmd
-{
-	local c="$1"
-	if [ $# -eq 0 ] || [ -z "$c" ]; then return 0; fi
-	which "$c" &> /dev/null
-	if [ $? -eq 0 ]; then return 0
-	else echo "command \"$c\" not found"; exit 1; fi
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o noclobber
+
+NAME="refrestore"
+VERSION="0.3.0"
+
+BROWSER_COMMAND="${BROWSER_COMMAND:-"firefox --new-tab"}"
+VERBOSE="${VERBOSE:-0}"
+
+function check_command {
+	if ! command -v "${1}" &>/dev/null; then
+		echo >&2 "no such command \"${1}\""
+		exit 1
+	fi
 }
 
-function open_link_files
-{
-	cd $1
+function print_version {
+	cat <<-EOF
+		${NAME} ${VERSION}
+		copyright (c) 2020-2023 Daniel Haase
+	EOF
+}
 
-	for name in ./*.{href,url}; do
-		if [ -f $name ]; then
-			link=$(cat $name)
-			echo "opening \"$link\"..."
-			firefox --new-tab "$link" &> /dev/null &
-			sleep 1
+function print_usage {
+	print_version
+	cat <<-EOF
+
+		usage:  ${NAME} [--verbose] [<resource>...]
+		        ${NAME} [--version | --help]
+
+		   <resource>...
+		      TODO: file, directory, URL
+
+		   -v | --verbose
+		      TODO
+
+		   -V | --version
+		      print version information and exit
+
+		   -h | --help
+		      print this usage description and exit
+
+	EOF
+}
+
+function is_link {
+	local -r regex="^https?://.+"
+
+	if [[ "${1}" =~ ${regex} ]]; then
+		return 0
+	fi
+
+	return 1
+}
+
+function open_link {
+	local -r link="${1}"
+
+	if ! is_link "${link}"; then
+		return 1
+	fi
+
+	if [[ ${VERBOSE} -gt 0 ]]; then
+		echo "opening \"${link}\"..."
+	fi
+
+	if ! eval "${expanded_command} ${link} &>/dev/null &"; then
+		return 1
+	fi
+
+	sleep 1
+}
+
+function open_link_files {
+	local -r directory="${1}"
+
+	if [[ ! -d "${directory}" || ! -r "${directory}" ]]; then
+		echo >&2 "failed to read directory \"${directory}\""
+		exit 3
+	fi
+
+	local link
+
+	for file in "${directory}/"*.{href,url}; do
+		if [[ -f "${file}" ]]; then
+			open_link "$(< "${file}")"
 		fi
 	done
-
-	cd $CWD
 }
 
-function open_file_links
-{
-	if [ ! -f $1 ] || [ -d $1 ]
-	then echo "file \"$1\" not found"; exit 1; fi
+function open_file_links {
+	local -r file="${1}"
 
-	while read link; do
-		if [ -z "$link" ] || [[ $link != http* ]]
-		then continue; fi
-		echo "opening \"$link\"..."
-		firefox --new-tab "$link" &> /dev/null &
-		sleep 1
-	done < "$1"
+	if [[ ! -f "${file}" ]]; then
+		echo >&2 "failed to read file \"${file}\""
+		exit 3
+	fi
+
+	while read -r link; do
+		open_link "${link}"
+	done <"${file}"
 }
 
-checkcmd "basename"
-checkcmd "cat"
-checkcmd "firefox"
-checkcmd "sleep"
+function process_argument {
+	local -r argument="${1}"
 
-if [ $# -eq 0 ]; then open_link_files $CWD
-elif [ $# -eq 1 ] &&
-     [[ "$1" == "-h" || "$1" == "--help" ]]; then
-	echo ""
-	echo "usage:  $(basename $0) <dir1> [<dir2>...]"
-	echo "        $(basename $0) -f <filename>"
-	echo "        $(basename $0) [-V | -h]"
-	echo ""
-	echo "  <dir1> [<dir2>...]"
-	echo "      open all \"*.href\" and \"*.url\" files contained"
-	echo "      the directories provided (<dir1>, <dir2>, ...)"
-	echo ""
-	echo "  -f <filename>"
-	echo "      open every hyperlink contained in file <filename>"
-	echo "      (one per line)"
-	echo ""
-	echo "  -V | --version"
-	echo "      print version information and exit"
-	echo ""
-	echo "  -h | --help"
-	echo "      print this help message and exit"
-	echo ""
-	echo "  if no parameters are given, every \"*.href\" and"
-	echo "  \"*.url\" file in current directory is opened"
-	echo ""
-elif [ $# -eq 1 ] &&
-     [[ "$1" == "-V" || "$1" == "--version" ]]; then
-	echo "refrestore version $VERSION"
-	echo "copyright (c) 2020 Daniel Haase"
-elif [ $# -eq 2 ] && [ "$1" == "-f" ]; then open_file_links $2
+	if is_link "${argument}"; then
+		open_link "${argument}"
+	elif [[ -f "${argument}" ]]; then
+		if [[ -r "${argument}" ]]; then
+			open_file_links "${argument}"
+		else
+			echo >&2 "file \"${argument}\" is not readable"
+			exit 3
+		fi
+	elif [[ -d "${argument}" ]]; then
+		if [[ -r "${argument}" ]]; then
+			open_link_files "${argument}"
+		else
+			echo >&2 "directory \"${argument}\" is not readable"
+			exit 3
+		fi
+	else
+		echo >&2 "invalid argument \"${argument}\""
+		exit 3
+	fi
+}
+
+declare -r command_name="${BROWSER_COMMAND%% *}"
+
+check_command "${command_name}"
+
+declare expanded_command
+
+expanded_command="$(command -v "${command_name}") "
+expanded_command+="${BROWSER_COMMAND/#${command_name} /}"
+
+check_command "cat"
+check_command "pwd"
+check_command "sleep"
+
+if [[ $# -eq 0 ]]; then
+	process_argument "$(pwd)"
+elif [[ $# -eq 1 ]]; then
+	case "${1}" in
+		-V | --version)
+			print_version
+			exit 0
+			;;
+		-h | --help)
+			print_usage
+			exit 0
+			;;
+		-v | --verbose)
+			VERBOSE=1
+			process_argument "$(pwd)"
+			;;
+		*)
+			process_argument "${1}"
+			;;
+	esac
 else
-	for arg in $@; do
-		if [ ! -d $arg ]
-		then echo "\"$arg\" is not a directory"
-		else open_link_files $arg; fi
+	while [[ $# -gt 0 ]]; do
+		process_argument "${1}"
 		shift
 	done
 fi
